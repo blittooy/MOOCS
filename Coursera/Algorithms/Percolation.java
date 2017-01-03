503a533ae62c038c7de71a7abcf08392b4ccbf58
Bradford Littooy <blittooy@ecorithm.com>
2017-01-03 09:07:13 -0800

    Started 'Algorithms'

---
+++ Coursera/Algorithms/Percolation.java
@@ -0,0 +1,37 @@
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
+public class Percolation {
+   public static int Percolation(int n){
+   // create n-by-n grid, with all sites blocke
+      int[][] a = new int[n][n];
+      return a;
+   }                
+   //public    void open(int row, int col){
+   // open site (row, col) if it is not open already
+   //}    
+   //public boolean isOpen(int row, int col){
+   // is site (row, col) open?
+     //  return False;
+   //}  
+   //public  boolean isFull(int row, int col){
+   // is site (row, col) full?
+     //  return False;
+   //}  
+   //public  int numberOfOpenSites(){
+   // number of open sites
+   //}       
+   //public void boolean percolates(){
+   // does the system percolate?
+   //}              
+
+   //public static void main(String[] args){
+   // test client (optional)
+   //}
+   public static void main(String args[]){
+      int[][] test = Percolation(7);
+      System.out.println(test);
+   }
+}
+
